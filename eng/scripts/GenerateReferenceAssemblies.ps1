5ad1bd08b6f86fa66aab9890dc09593c604d3070
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-05-20 21:23:49 -0700

    Adjust generation scripts to explicitly choose the MSBuild engine - ensure native assets are included in GenerateReferenceAssemblies.ps1 build - clean up the global state tools.ps1 corrupts

--- eng/scripts/GenerateReferenceAssemblies.ps1
+++ eng/scripts/GenerateReferenceAssemblies.ps1
@@ -1,10 +1,21 @@
 param(
     [switch]$ci
 )
-$ErrorActionPreference = 'stop'
 
+# GenAPI with `dotnet msbuild` doesn't consistently order attributes in Microsoft.AspNetCore.Mvc.TagHelpers.netcoreapp.cs
+$msbuildEngine = 'vs'
+
+$ErrorActionPreference = 'stop'
 $repoRoot = Resolve-Path "$PSScriptRoot/../.."
 
-& "$repoRoot\eng\common\msbuild.ps1" -ci:$ci "$repoRoot/eng/CodeGen.proj" `
+try {
+  & "$repoRoot\eng\common\msbuild.ps1" -ci:$ci "$repoRoot/eng/CodeGen.proj" `
     /t:GenerateReferenceSources `
-    /bl:artifacts/log/genrefassemblies.binlog
+    /bl:artifacts/log/genrefassemblies.binlog `
+    /p:BuildNative=true
+} finally {
+    Remove-Item variable:global:_BuildTool -ErrorAction Ignore
+    Remove-Item variable:global:_DotNetInstallDir -ErrorAction Ignore
+    Remove-Item variable:global:_ToolsetBuildProj -ErrorAction Ignore
+    Remove-Item variable:global:_MSBuildExe -ErrorAction Ignore
+}
